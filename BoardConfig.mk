79736ee7ba56ae4ed3e4bb2be0103f2406bbbffd
Kyle Laker <kalaker@teamblueridge.com>
2013-01-21 23:47:27 -0500

    Change to SickleSoftware kernel source

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,7 +94,7 @@ BOARD_FLASH_BLOCK_SIZE := 262144
 
 TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
 
-TARGET_KERNEL_SOURCE := kernel/htc/primoc
+TARGET_KERNEL_SOURCE := kernel/htc/msm7x30
 TARGET_KERNEL_CONFIG := primoc_defconfig
 TARGET_PREBUILT_KERNEL := device/htc/primoc/prebuilt/root/kernel
 
